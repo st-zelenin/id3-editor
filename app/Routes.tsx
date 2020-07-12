2e10722686052bd2797b0506689fb0d177970905
Stepan Zelenin <stepanzelenin@Stepans-MacBook-Pro.local>
2020-07-12 21:14:47 +0530

    fix(empty prod page): fixed the issue with missing react module on prod

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -1,10 +1,11 @@
 /* eslint react/jsx-props-no-spreading: off */
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+
 import routes from './constants/routes.json';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
 import EditorPage from './containers/Editor';
+import HomePage from './containers/HomePage';
 
 // Lazily load routes and code split with webpacck
 const LazyCounterPage = React.lazy(() =>
